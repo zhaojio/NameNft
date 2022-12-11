661624ca3258be8032773e57e9103a5f6275962b
赵杰 <1115500401@qq.com>
2022-12-11 17:13:33 +0800

    frontend release

--- copy.sh
+++ copy.sh
@@ -1,3 +1,5 @@
 cp .env frontend/assets
 cp artifacts/contracts/nft-name/NftName.sol/NameNft.json frontend/assets
+cp artifacts/contracts/nft-name/NftName.sol/NameNft.json frontend/release/assets/assets
 cp info/local.json frontend/assets
+cp info/local.json frontend/release/assets/assets
